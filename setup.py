6edd43b24ec54d0be752e1fd8c9ab28ccc53cde9
agbleze <lagbleze@st.ug.edu.gh>
2023-07-15 20:53:32 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='image_background_changer_api',
+      version='0.0.1',
+      description='''This is a deep learning API for changing the background of images
+                    to what the user prefers.
+                    ''',
+      author='Agbleze Linus',
+      license='MIT',
+      packages=['image_background_changer_api'],
+      zip_safe=False
+      )
